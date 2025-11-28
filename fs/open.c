1a5c48826032e7f815cd1b44bef37fae5b39e86f
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-11-28 09:25:04 +0800

    fs/kernel: update manual hooks to v1.6

--- fs/open.c
+++ fs/open.c
@@ -355,8 +355,9 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 }
 
 #ifdef CONFIG_KSU
-extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
-			        int *flags);
+__attribute__((hot)) 
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
+				int *mode, int *flags);
 #endif
 
 /*
@@ -377,7 +378,10 @@ SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
+<<<<<<< HEAD
 
+=======
+>>>>>>> 87e6c1fcbd9c (fs/kernel: update manual hooks to v1.6)
 #ifdef CONFIG_KSU
 	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 #endif
